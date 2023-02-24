4d2a96bb5540384d1700e0e650ca2a40c885d28d
Yash86400-design <knowledge86400@gmail.com>
2023-02-24 13:06:32 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div className='app__container'>
+      <h2>Alpaca Image Generator</h2>
+    </div>
+  );
+}
+
+export default App;
